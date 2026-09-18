dc3baa5bbd982dad479887cbdd44970a7e62662b
Aditya Kumar Gupta <adityakrgupta9008@gmail.com>
2026-09-18 12:47:52 +0530

    Put GCD

---
+++ Number_Problems/gcd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<< "Enter a : ";
+    cin>>a;
+    cout<<"Enter b : ";
+    cin>>b;
+    int gcd =1;
+    for(int i=1; i<=min(a,b);i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+        
+    }
+    cout<<"GCD : "<<gcd;
+
+    return 0;
+
+}
